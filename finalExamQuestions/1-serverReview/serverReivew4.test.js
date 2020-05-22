f3801fb93888818d38be4427b0d62b8971f443f1
niccoryan0 <niccoryan0@gmail.com>
2020-05-22 13:09:31 -0700

    scAFFOLDING

---
+++ finalExamQuestions/1-serverReview/serverReivew4.test.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/* ------------------------------------------------------------------------------------------------
+
+Complete the createServer function to include routes
+a GET request to / should respond with status of 200
+a DELETE request to /things/1 should respond with status of 405
+any other route should return status of 404
+------------------------------------------------------------------------------------------------ */
+const createServer = () => {
+  const express = require('express');
+  const app = express();
+
+  // routes go here
+
+  var server = app.listen(3000, function () {
+    var port = server.address().port;
+    console.log('Example app listening at port', port);
+  });
+  return server;
+};
+
+describe('Testing challenge', function () {
+
+  const request = require('supertest');
+
+  let server;
+
+  beforeEach(function () {
+    server = createServer();
+  });
+
+  afterEach(function () {
+    server.close();
+  });
+
+  test('responds to /', function testSlash(done) {
+    request(server)
+      .get('/')
+      .expect(200, done);
+  });
+
+  test('responds to /things/1', function testSlash(done) {
+    request(server)
+      .delete('/things/1')
+      .expect(405, done);
+  });
+
+  test('404 everything else', function testPath(done) {
+    request(server)
+      .get('/foo/bar')
+      .expect(404, done);
+  });
+});
+
